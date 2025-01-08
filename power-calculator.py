8f6122dd7c092cb794489b95e7f2c9c8d66bc124
jrbartosch <jbartosch+jed@gmail.com>
2025-01-08 08:43:26 +0600

    Add files via upload

--- power-calculator.py
+++ power-calculator.py
@@ -1,3 +1,4 @@
-num = float(input('Please Enter a Number: '))
-exp = float(input('Please Enter a Number: '))
-print (num**exp)
+num = float(input('Please Enter the Base Number: '))
+exp = float(input('Please Enter the Exponent: '))
+result = num ** exp
+print(f'{num} to the power of {exp} is {result}.')
